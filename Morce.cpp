86d9218b6aa3c1c5a50f9b46f1c984d122473122
Victor Arriagada <veamchile@hotmail.com>
2018-04-21 18:01:17 -0300

    version 2
    version 2

--- Morce.cpp
+++ Morce.cpp
@@ -2,6 +2,7 @@
   Morse.cpp - Library for flashing Morse code.
   Created by David A. Mellis, November 2, 2007.
   Released into the public domain.
+  segunda actualizacion
 */
  
 #include "Arduino.h"
--- Morse.h
+++ Morse.h
@@ -2,6 +2,7 @@
   Morse.h - Library for flashing Morse code.
   Created by David A. Mellis, November 2, 2007.
   Released into the public domain.
+  Segunda actualizacion
 */
 #ifndef Morse_h
 #define Morse_h
